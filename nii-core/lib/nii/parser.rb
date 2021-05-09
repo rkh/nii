f5fffb857a94f68f0a1a4c2b6af25251e9838861
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2021-05-09 18:42:13 +0200

    core: general purpose message parser

--- nii-core/lib/nii/parser.rb
+++ nii-core/lib/nii/parser.rb
@@ -8,6 +8,7 @@ module Nii
     autoload :Gettext,    'nii/parser/gettext'
     autoload :JSON,       'nii/parser/json'
     autoload :Locale,     'nii/parser/locale'
+    autoload :Message,    'nii/parser/message'
     autoload :Number,     'nii/parser/number'
     autoload :Properties, 'nii/parser/properties'
     autoload :YAML,       'nii/parser/yaml'
@@ -17,6 +18,7 @@ module Nii
     def self.json(source,        **options) = JSON       .parse(source, **options)
     def self.gettext(source,     **options) = Gettext    .parse(source, **options)
     def self.locale(source,      **options) = Locale     .parse(source, **options)
+    def self.message(source,     **options) = Message    .parse(source, **options)
     def self.number(source,      **options) = Number     .parse(source, **options)
     def self.properties(source,  **options) = Properties .parse(source, **options)
     def self.yaml(source,        **options) = YAML       .parse(source, **options)
---
+++ nii-core/lib/nii/parser/message.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+module Nii::Parser
+  module Message
+    extend self
+
+    ParseError    = Nii::Errors::ParseError
+    OPEN          = /[\$%#]?\{\s*/
+    CLOSE         = /\s*\}/
+    UNICODE       = /\\u([0-9a-fA-F]{4})|\\(U[0-9a-fA-F]{6})|\\[uU]\{[(0-9a-fA-F)]+\}/
+    COMMA         = /\s*,\s*/
+    ESCAPE        = /\\(.)/
+    NUMBER        = /\-?\d+/
+    IDENTIFIER    = /\$?([a-z][\w\.]*)/
+    OPTION        = /([a-z]\w*):/
+    MATCHER       = /\s*(?:([<=>])(#{NUMBER})|(\w+))\s*\{/
+    MATCHER_CLOSE = /\}/
+    SKELETON      = /::([^\,\}]*)/
+    ESCAPES       = { ?n => ?\n, ?t => ?\t, ?r => ?\r, ?f => ?\f, ?v => ?\v, ?s => ?\s }
+
+    def parse(input, **, &done)
+      input  = StringScanner.new(input)
+      tokens = []
+      append(tokens, scan_element(input).dup) until input.eos?
+      tokens
+    end
+
+    private
+
+    def append(list, item)
+      list = list.last if item.is_a? String and list.last.is_a? String
+      list << item
+    end
+
+    def scan_element(input) = placeable(input) || literal(input)
+
+    def literal(input)
+      return input.captures.join.hex.chr('UTF-8') if input.scan(UNICODE)
+      return ESCAPES.fetch(input[1], input[1])    if input.scan(ESCAPE)
+      input.getch
+    end
+
+    def placeable(input)
+      attempt(input) do
+        return unless input.scan(OPEN)
+        if nested = placeable(input)
+          raise ParseError unless input.scan(CLOSE)
+          return nested
+        end
+        raise ParseError unless identifier = identifier(input)
+        tokens = [identifier]
+        until input.scan(CLOSE)
+          raise ParseError unless input.scan(COMMA)
+          element = icu_select(input) || [skeleton(input) || identifier(input)]
+          tokens += element
+        end
+        tokens
+      end
+    end
+
+    def skeleton(input)
+      input[1] if input.scan(SKELETON)
+    end
+
+    def identifier(input)
+      return Number.parse(input.matched) if input.scan(NUMBER)
+      input[1].to_sym if input.scan(IDENTIFIER)
+    end
+
+    def icu_select(input)
+      attempt(input) do
+        variants = []
+        options  = {}
+        options[input[1].to_sym] = identifier(input) while input.scan(OPTION)
+        while input.scan(MATCHER)
+          condition = input[1] if input[1] != '='
+          condition = condition&.to_sym || :==
+          element   = [condition, input[2] ? Number.parse(input[2]) : input[3], []]
+          until input.scan(MATCHER_CLOSE)
+            raise ParseError if input.eos?
+            append(element.last, scan_element(input).dup)
+          end
+          variants << element
+        end
+        result = [options, *variants].compact.reject(&:empty?)
+        result if result.any?
+      end
+    end
+
+    def attempt(input)
+      position = input.pos
+      yield
+    rescue ParseError
+      input.pos = position
+      nil
+    end
+  end
+end
