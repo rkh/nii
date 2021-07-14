44f1a79a6420735ccd796c58475d273602cb67c9
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2021-07-14 09:42:02 +0200

    core: add tests for formatting money

--- nii-core/test/formatters/test_money_formatter.rb
+++ nii-core/test/formatters/test_money_formatter.rb
@@ -1,5 +1,20 @@
 require_relative '../setup'
 require 'money'
 
+# to avoid warning
+Money.rounding_mode = BigDecimal::ROUND_HALF_UP
+
 class TestMoneyFormatter < Minitest::Test
+  def setup
+    @context = Nii::Context.new :en
+  end
+
+  def test_format_money
+    assert_equal '$1,200.43', @context.format(Money.usd(1200_43))
+    assert_equal '₿100.00', @context.format(Money.from_amount(100, 'BTC'))
+  end
+
+  def test_spellout_money
+    assert_equal 'one hundred fifty US dollars', @context.spellout(Money.usd(150_00))
+  end
 end
